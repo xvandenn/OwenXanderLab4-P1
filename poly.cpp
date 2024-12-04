820e4a76197287f65daa6dbad19d11b7976230a1
owen <owensemeter@gmail.com>
2024-12-04 18:01:58 -0500

    testing with 8 threads

--- poly.cpp
+++ poly.cpp
@@ -117,7 +117,7 @@ polynomial polynomial::operator*(const polynomial& other) const
 	product.pop();
 
 	auto iter = p.begin();
-	int threadSize = (p.size() + 999) / 1000;
+	int threadSize = (p.size() + 7) / 8;
 
 	//testing out lambda
 	auto mux = [&/*using ref instead of copy this time*/](auto start, auto end)
@@ -144,7 +144,7 @@ polynomial polynomial::operator*(const polynomial& other) const
 		mutex.unlock();
 	};
 
-	for(int i = 0; i < 1000; i++)
+	for(int i = 0; i < 8; i++)
 	{
 		auto start = iter;
 		int j = 0;
